afb80906ffadbb73c069ea8387fe58c104745804
H1Rishav <hrishavkhanal1@gmail.com>
2026-08-27 20:03:01 +0530

    another commit for CycleFinding.cpp

---
+++ cses/CycleFinding.cpp
@@ -0,0 +1,154 @@
+#include <bits/stdc++.h>
+using namespace std;
+int findpar(int node,vector<int>&par){
+    if(node==par[node])return node;
+    return par[node]=findpar(par[node],par);
+}
+void unionf(int u,int v,vector<int>&size,vector<int>&par){
+    int u_par=findpar(u,par);
+    int v_par=findpar(v,par);
+    if(size[u_par]>size[v_par])
+    {
+        size[u_par]+=size[v_par];
+        par[v_par]=u_par;
+    }
+    else{
+        size[v_par]+=size[u_par];
+        par[u_par]=v_par;
+    }
+}
+int start,endf;
+bool dfs(int node,vector<vector<pair<int,int>>>&adj,vector<int>&vis,vector<int>&par,long long sum){
+    vis[node]=1;
+    for(auto it:adj[node])
+    {
+        int secnode=it.first;
+        int weight=it.second;
+        if(vis[secnode]&&it.first!=par[node]&&sum+weight<0)
+        {
+            start=secnode;
+            endf=node;
+            return true;
+        }
+        par[secnode]=node;
+        dfs(secnode,adj,vis,par,sum+weight);
+    }
+    return false;
+
+}
+void solve(){
+    int n,m;
+    cin>>n>>m;
+   // cout<<n<<m<<q;
+
+   vector<vector<pair<int,int>>>adj(n);
+   //vector<vector<long long>>edges;   
+   
+  start=-1,endf=-1;
+   for(int i=0;i<m;i++)
+   {
+        int a,b,c;
+        cin>>a>>b>>c;a--;b--;
+        adj[a].push_back({b,c});
+        //indeg[b]++;
+        //adj[a].push_back(b);
+   }
+   vector<int>par(n,0);
+   par[0]=0;
+   vector<int>vis(n,0);
+   if(dfs(0,adj,vis,par,0LL))
+   {
+    cout<<"NO"<<endl;
+   }
+   else{
+    cout<<"YES"<<endl;
+    cout<<start+1<<" ";
+    while(start!=endf){
+        cout<<endf+1<<" ";
+        endf=par[endf];
+    }
+    cout<<start+1<<" ";
+
+   }
+
+
+   
+    
+}
+// void solve()
+// {
+//    int n,m;
+//    cin>>n>>m;
+//    vector<vector<int>>adj(n);
+//    for(int i=0;i<m;i++)
+//    {
+//     int a,b;
+//     cin>>a>>b;a--;b--;
+//     adj[a].push_back(b);
+//     adj[b].push_back(a);
+//    }
+
+//    //cycle detection via bfs
+//    queue<int>q;
+//    vector<int>vis(n,0);
+//    q.push(0);
+//    vis[0]=1;
+//    vector<int>par(n,0);
+//    int anotherst=-1;
+//    int start=-1;
+//    while(!q.empty()){
+//     auto it=q.front();
+//     q.pop();
+    
+//     for(auto nodes:adj[it])
+//     {
+//         if(nodes==par[it])continue;
+
+//         if(vis[nodes]&&nodes!=par[it]) {
+//             start=nodes;
+//             anotherst=it;
+//            break;
+//             }
+//             vis[nodes]=1;
+//             par[nodes]=it;
+//         q.push(nodes);
+//     }
+
+//    }
+//    if(start==-1)
+//    {
+//     cout<<"IMPOSSIBLE";
+//    }
+//    else{
+
+//     //need to do dfs
+//     set<int>st;
+//     st.insert(start);
+//     string s="";
+//     s+=to_string(start);
+//     while(par[start]!=start){
+//         s+=to_string(par[start]);
+//         st.insert(par[start]);
+//         start=par[start];
+//     }
+//     string s1="";
+//     while(true){
+//         if(st.find(anotherst)!=st.end())
+//         {
+//         reverse(s1.begin(),s1.end());
+//         s+=s1;
+//         s+=s[0];
+//         break;
+//         }
+//         s1+=to_string(anotherst);
+//         anotherst=par[anotherst];
+//     }
+//     cout<<s1<<endl;
+//    }
+// }
+int main()
+{
+    int t = 1;
+    while (t--)
+        solve();
+}
